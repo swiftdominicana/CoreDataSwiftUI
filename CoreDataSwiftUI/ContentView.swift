1be35d0e9da92f1e374d7317f9b8e8e88e52d789
Libranner <libranner@gmail.com>
2020-02-15 02:41:26 +0100

    UI changes

--- CoreDataSwiftUI/ContentView.swift
+++ CoreDataSwiftUI/ContentView.swift
@@ -31,52 +31,85 @@ struct ContentView: View {
         if let value = NumberFormatter().number(from: $0) {
           self.priceRawValue = value.doubleValue
         }
-      }
+    }
     )
   }
   
   var body: some View {
-    VStack {
+    NavigationView {
       VStack {
-        List(devices, id:\.self) { device in
-          VStack(alignment: .leading, spacing: 3) {
-            Text(device.name ?? "N/A")
-            Text(String(format: "%.2f", device.price))
-              .font(.subheadline)
-              .foregroundColor(.gray)
+        VStack {
+          List {
+            ForEach(devices, id:\.self) { device in
+              VStack(alignment: .leading, spacing: 3) {
+                Text(device.name ?? "N/A")
+                Text(String(format: "%.2f", device.price))
+                  .font(.subheadline)
+                  .foregroundColor(.gray)
+              }
+            }.onDelete(perform: deleteDevice)
           }
-        }
-        
-        Button("Add new Device") {
-          self.showForm = true
-        }
-        
-        Button("Guardar") {
-          if self.moc.hasChanges {
-            do {
-              try self.moc.save()
-            }
-            catch {
-              print(error.localizedDescription)
-            }
-          }
-        }
-      }
-    }.sheet(isPresented: $showForm) {
-      VStack(spacing: 30) {
-        TextField(" Enter device name", text: self.$deviceName)
-        TextField(" Enter device price", text: self.price)
-        Button("Add") {
-          let device = Device(context: self.moc)
-          device.name = self.deviceName
-          device.price = self.priceRawValue
           
-          self.deviceName = ""
-          self.priceRawValue = 0
-          self.showForm = false
+          VStack(spacing: 30) {
+            Button("Add new Device") {
+              self.showForm = true
+            }
+            
+            Button("Guardar") {
+              if self.moc.hasChanges {
+                do {
+                  try self.moc.save()
+                }
+                catch {
+                  print(error.localizedDescription)
+                }
+              }
+            }
+          }
         }
-      }.padding([.leading, .trailing], 40)
+      }.navigationBarTitle(Text("Devices List"))
+        .sheet(isPresented: $showForm) {
+          VStack(alignment: .leading, spacing: 30) {
+            Text("Name")
+              .foregroundColor(.gray)
+            TextField(" Enter device name", text: self.$deviceName)
+              .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Price")
+              .foregroundColor(.gray)
+            TextField(" Enter device price", text: self.price)
+              .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+              let device = Device(context: self.moc)
+              device.name = self.deviceName
+              device.price = self.priceRawValue
+              
+              self.deviceName = ""
+              self.priceRawValue = 0
+              self.showForm = false
+            }) {
+              HStack(alignment: .center) {
+                Text("Add")
+                  .frame(minWidth: 0, maxWidth: .infinity)
+                  .padding()
+                  .background(Color.blue)
+                  .foregroundColor(.white)
+                  .cornerRadius(40)
+              }
+            }
+          }.padding([.leading, .trailing], 40)
+      }
     }
+  }
+  
+  func deleteDevice(at offsets: IndexSet) {
+    for offset in offsets {
+      let device = devices[offset]
+      moc.delete(device)
+    }
+    
+    try? moc.save()
   }
   
 }
