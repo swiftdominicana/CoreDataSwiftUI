d47f3e8f1b221fafa81af66e71f721a9bd35e1ff
Libranner <libranner@gmail.com>
2020-02-15 01:11:09 +0100

    Initial Commit

---
+++ CoreDataSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataSwiftUI
+//
+//  Created by Libranner Leonel Santos Espinal on 15/02/2020.
+//  Copyright © 2020 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
